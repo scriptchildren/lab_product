1b98c374f203b30c996bd3c4f7ebef0d65fb9757
scriptchildren <46424849+scriptchildren@users.noreply.github.com>
2021-12-29 16:03:46 +0900

    Add files via upload
    add extract pos info func

--- passive_cp.py
+++ passive_cp.py
@@ -8,17 +8,34 @@ def isPassive(sentence):
     aux = ['do', 'did', 'does', 'have', 'has', 'had']                                  # NLTK tags "do" and "have" as verbs, which can be misleading in the following section.
     app = ['alarmed', 'aggravated', 'amazed', 'amused', 'annoyed', 'astonished', 'astounded', 'bewildered', 'bored', 'captivated', 'challenged', 'charmed', 'comforted', 'concerned', 'confused', 'convinced', 'depressed', 'devastated', 'disappointed', 'discouraged', 'disgusted', 'distressed', 'disturbed', 'embarrassed', 'enchanted', 'encouraged', 'energise', 'entertained', 'exasperated', 'excited', 'exhausted', 'fascinated', 'flattered', 'frightened', 'frustrated', 'fulfilled', 'gratified', 'horrified', 'humiliated', 'inspired', 'insulted', 'interested', 'intrigued', 'irritated', 'mystified', 'moved', 'overwhelmed', 'perplexed', 'perturbed', 'pleased', 'puzzled', 'relaxed', 'satisfied', 'shocked', 'sickened', 'soothed', 'surprised', 'tempted', 'terrified', 'threatened', 'thrilled', 'tired', 'touched', 'troubled', 'unnerved', 'unsettled', 'upset', 'worried']
     words = nltk.word_tokenize(sentence)
+    #i would like to do  words.remove(-.,\n?)
+    print(words)
+    #def word_clean(string):
+    #    for char in '-.,\n':
+    #        string=string.replace(char,' ')
+    #    string = string.lower()
+    #    word_list = string.split()
+    #    return(word_list)
     tokens = nltk.pos_tag(words)
+    #extract pos tagging information
+    out = [lis[1] for lis in tokens]
+    print(out) 
+    #dict = {}
+    #for element in tokens:
+    #    dict[element] = dict.get(element, 0) + 1
+    #word_freq = []
+    #cnt = 0
+    #for key, value in dict.items():
+    #    word_freq.append((value, key))
+    #word_freq.sort(reverse=True)
+    #print(word_freq)
     tags = [i[1] for i in tokens]
-    if tags.count('VBN') == 0:                                                            # no PP, no passive voice.
+    if tags.count('VBN') == 0: # no PP, no passive voice.
         return False
-    elif tags.count('VBN') == 1 and 'been' in words:                                    # one PP "been", still no passive voice.
+    elif tags.count('VBN') == 1 and 'been' in words:  # one PP "been", still no passive voice.
         return False
-    #elif tags.count('VBN') == 1 and app in words:
-        #return False
     else:
-        pos = [i for i in range(len(tags)) if tags[i] == 'VBN' and words[i] != 'been' and words[i] not in app]  # gather all the PPs that are not "been".
-        #print(len(tags)) #is sum of words include '.,?'
+        pos = [i for i in range(len(tags)) if tags[i] == 'VBN' and words[i] != 'been' and words[i] not in app]  # gather all the PPs that are not "been" and are not adjective.
         for end in pos:
             chunk = tags[:end]
             start = 0
@@ -30,20 +47,12 @@ def isPassive(sentence):
             sentchunk = words[start:end] #words chunk 
             tagschunk = tags[start:end]
             verbspos = [i for i in range(len(tagschunk)) if tagschunk[i].startswith('V')] # get all the verbs in between
-            count = 0 
             if verbspos != []:   # if there are no verbs in between, it's not passive
                 for i in verbspos:
                    if sentchunk[i].lower() not in beforms and sentchunk[i].lower() not in aux:  # check if they are all forms of "be" or auxiliaries such as "do" or "have".
                        break
-            #if adjectivepos != []:
-            #    for i in adjectivepos:
-            #        if sentchunk[i].lower() not in app:
-            #           break
-                   #if sentchunk[i].lower() in app:
-                      # break
                 else:
-                    #count = count + 1
-                    return True 
+                    return True
     return False
 
 
