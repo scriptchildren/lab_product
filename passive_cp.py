6a8493f04f515ad65b28cc8a0370d9a563e0e383
scriptchildren <46424849+scriptchildren@users.noreply.github.com>
2022-01-27 23:44:04 +0900

    Add files via upload
    this is latest version of passive voice

--- passive_cp.py
+++ passive_cp.py
@@ -9,21 +9,12 @@ from nltk.corpus import stopwords
 #stopwords.words('english')
 #en_stops = set(stopwords.words('english'))
 
-samples = '''I like being hunted.
-The man is being hunted.
-Don't be frightened by what he said.
-I assume that you are not informed of the matter.
-Please be advised that the park is closing soon.
-The book will be released tomorrow.
-We're astonished to see the building torn down.
-The hunter is literally being chased by the tiger.
-He has been awesome since birth.
-She has been beautiful since birth.
-I am bored.
-'''
-
-file = open("../source/Active_vs_passive.txt", "r")
-sentence = file.read()
+#file = open("../source/Active_vs_passive.txt", "r")
+file1 = open("../source/biden_speech.txt")
+file2 = open("../source/trump_speech.txt")
+Q = file1.read()
+K = file2.read()
+#sentence = file.read()
 
 #pp stands for preprocess
 def pp(sentence):
@@ -48,19 +39,21 @@ def make_list(sentence):
     labels = list(most_common_words)
     return most_common_words,percentage,fd,pos_freq
 
-lists = make_list(sentence)
-
+lists1 = make_list(Q)
+lists2 = make_list(K)
+#lists_test = make_list(sentence)
 #colors = ['yellowgreen', 'lightgreen', 'darkgreen', 'gold', 'red', 'lightsalmon', 'darkred']
 #plt.pie(percentage, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True,  startangle=90)
 #plt.axis('equal')
 #plt.legend
 
 #print this line pos freq ranking
-def make_ranking(mcw):
+def make_ranking(mcw, fd):
     for rank, word in enumerate(mcw):
-        print("%3d %6.2f%% %s" % (rank + 1, lists[2].freq(word) * 100, word))
+        print("%3d %6.2f%% %s" % (rank + 1, fd.freq(word) * 100, word))
 
-make_ranking(lists[0])
+make_ranking(lists1[0], lists1[2])
+make_ranking(lists2[0], lists2[2])
 #piechart(sentence)
 
 # respect https://ai-inter1.com/python-multichart/
@@ -90,26 +83,32 @@ def piechart(x,y,c):
      plt.rcParams['font.size'] = 13.0
 
      #グラフ描画
-     plt.pie(y, labels=x, counterclock=True,autopct="%1.1f%%",colors=c) #円グラフの描画
+     plt.pie(y, labels=x, counterclock=True,autopct="%1.1f%%",colors=c,normalize=True) #円グラフの描画
 
-#データ準備
-pie_colors = ["r", "c", "b", "m", "y"] # 製品毎の色指定
+def plot_chart(x,y,z):
+    #データ準備
+    pie_colors = ["r", "c", "b", "m", "y"] # 製品毎の色指定
 
-#グラフの描画
-plt.figure(figsize=(25,10)) #描画領域の指定
-plt.subplots_adjust(wspace=0.2, hspace=0) #間隔指定
+    #グラフの描画
+    plt.figure(figsize=(25,10)) #描画領域の指定
+    plt.subplots_adjust(wspace=0.2, hspace=0) #間隔指定
 
-plt.subplot(1,2,1) #グラフ描画位置の指定
-histchart(lists[0], lists[3])
+    plt.subplot(1,2,1) #グラフ描画位置の指定
+    #histchart(lists[0], lists[3])
+    histchart(x,z)
 
-plt.subplot(1,2,2) #グラフ描画位置の指定
-piechart(lists[0], lists[1], pie_colors)
-plt.show()
-#cnt = Counter(out)
-#print(cnt.most_common())
+    plt.subplot(1,2,2) #グラフ描画位置の指定
+    #piechart(lists[0], lists[1], pie_colors)
+    piechart(x,y,pie_colors)
+    plt.show()
+    #cnt = Counter(out)
+    #print(cnt.most_common())
+
+plot_chart(lists1[0], lists1[1], lists1[3])
+plot_chart(lists2[0], lists2[1], lists2[3])
+#plot_chart(lists_test[0], lists_test[1], lists_test[3])
 
 def isPassive(sentence):
-    cntpos = 0
     beforms = ['am', 'is', 'are', 'been', 'was', 'were', 'be', 'being']               # all forms of "be"
     aux = ['do', 'did', 'does', 'have', 'has', 'had']                                  # NLTK tags "do" and "have" as verbs, which can be misleading in the following section.
     app = ['alarmed', 'aggravated', 'amazed', 'amused', 'annoyed', 'astonished', 'astounded', 'bewildered', 'bored', 'captivated', 'challenged', 'charmed', 'comforted', 'concerned', 'confused', 'convinced', 'depressed', 'devastated', 'disappointed', 'discouraged', 'disgusted', 'distressed', 'disturbed', 'embarrassed', 'enchanted', 'encouraged', 'energise', 'entertained', 'exasperated', 'excited', 'exhausted', 'fascinated', 'flattered', 'frightened', 'frustrated', 'fulfilled', 'gratified', 'horrified', 'humiliated', 'inspired', 'insulted', 'interested', 'intrigued', 'irritated', 'mystified', 'moved', 'overwhelmed', 'perplexed', 'perturbed', 'pleased', 'puzzled', 'relaxed', 'satisfied', 'shocked', 'sickened', 'soothed', 'surprised', 'tempted', 'terrified', 'threatened', 'thrilled', 'tired', 'touched', 'troubled', 'unnerved', 'unsettled', 'upset', 'worried']
@@ -124,7 +123,7 @@ def isPassive(sentence):
     pos_freq = []
     for key, value in dict.items():
         pos_freq.append((key, value))
-    print(pos_freq)
+    #print(pos_freq)
     tags = [i[1] for i in tokens]
     if tags.count('VBN') == 0: # no PP, no passive voice.
         return False
@@ -150,19 +149,20 @@ def isPassive(sentence):
                    if sentchunk[i].lower() not in beforms and sentchunk[i].lower() not in aux:  # check if they are all forms of "be" or auxiliaries such as "do" or "have".
                        break
                 else:
-                    cntpos += 1
                     return True
     return False
-
 
 if __name__ == '__main__':
     # "awesome" is wrongly tagged as PP. So the sentence gets a "True".
     #amount_pos(samples)
-    cnt = 0
-    cnt = sentence.count('.')
-    print("This document has " + str(cnt) + " sentence")
-    sents = nltk.sent_tokenize(sentence)
-    for sent in sents:
-        print(sent + '--> %s' % isPassive(sent))
-
+    def p_passive(sentence):
+        cnt = 0
+        cnt = sentence.count('.')
+        print("This document has " + str(cnt) + " sentence")
+        sents = nltk.sent_tokenize(sentence)
+        for sent in sents:
+            print(sent + '--> %s' % isPassive(sent))
+            
+    p_passive(Q)
+    p_passive(K)
 #plan to add function count sentence have passive voice and caluculate freaquence each pos tags 
